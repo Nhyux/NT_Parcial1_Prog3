bff4dfb1894c2284a26a43f5bc31824c78d3f679
Alan Ariel Rajakovic <alanariel02@gmail.com>
2026-05-21 18:28:06 -0300

    NT_Parcial1_Prog3

---
+++ NT_Parcial1_Prog3/src/main/java/unlar/edu/ar/NT_Parcial1_Prog3/NtParcial1Prog3Application.java
@@ -0,0 +1,13 @@
+package unlar.edu.ar.NT_Parcial1_Prog3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NtParcial1Prog3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NtParcial1Prog3Application.class, args);
+	}
+
+}
---
+++ NT_Parcial1_Prog3/src/test/java/unlar/edu/ar/NT_Parcial1_Prog3/NtParcial1Prog3ApplicationTests.java
@@ -0,0 +1,13 @@
+package unlar.edu.ar.NT_Parcial1_Prog3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NtParcial1Prog3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
